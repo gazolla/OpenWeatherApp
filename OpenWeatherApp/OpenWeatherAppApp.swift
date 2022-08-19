850227a6269c2a49bbe7b13d2a7d23f497b2f3a3
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-08-19 17:25:08 -0300

    Initial Commit

---
+++ OpenWeatherApp/OpenWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherAppApp.swift
+//  OpenWeatherApp
+//
+//  Created by sebastiao Gazolla Costa Junior on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
